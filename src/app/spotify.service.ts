a8ef6dae677360333ce21850e5ab663cce1c4ef1
Robert-Jung <rjinjung@gmail.com>
2017-07-31 22:06:55 -0700

    Adding spotify service.

---
+++ src/app/spotify.service.ts
@@ -0,0 +1,16 @@
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
+
+export class SpotifyService{
+    constructor(public http: Http) {
+    }
+
+    searchTrack(query: string) {
+        let params: string = [
+            `q=${query}`,
+            `type=track`
+        ].join("&");
+        let queryURL: string = `https://api.spotify.com/v1/search?${params}`;
+        return this.http.request(queryURL).map(res => res.json());
+    }
+}
